663ac4b1f51e2ed4d2f362c66b4a57b9affcdef9
Dan Mohl <dmohl@yahoo.com>
2010-05-03 00:42:31 -0500

    Continued working on the simple Querly integration example

--- Ferly/Program.fs
+++ Ferly/Program.fs
@@ -2,14 +2,18 @@
 
 open System
 open Otp
-    
+
 Console.Write ":>"  
 let mutable input = Console.ReadLine()  
 
-let remoteCall (sql:string) = 
-    let cNode = new OtpSelf("querlyclientnode")
+let erlangCookie = "supersecretcookie"
+
+let connection =
+    let cNode = new OtpSelf("querlyclientnode", erlangCookie)
     let sNode = new OtpPeer("querly@dmohl-PC")
-    let connection = cNode.connect(sNode)
+    cNode.connect(sNode)
+
+let RemoteCall (sql:string) = 
     let arguments = [|new Erlang.String(sql)|] : Erlang.Object[]
     do connection.sendRPC("querly_client", "sql_query", arguments) 
     connection.receiveRPC().ToString()
@@ -18,7 +22,7 @@ while input <> "quit"
     do  
     if input <> "quit"   
     then
-        let result = remoteCall input  
+        let result = RemoteCall input  
         Console.Write(result)
         Console.Write "\n:>"   
         input <- Console.ReadLine()
