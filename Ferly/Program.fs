0bd697b9a38c6f7e6a7552caad9f15aa6f3ad97d
Dan Mohl <dmohl@yahoo.com>
2010-05-03 00:00:45 -0500

    Created

---
+++ Ferly/Program.fs
@@ -0,0 +1,25 @@
+﻿module Ferly
+
+open System
+open Otp
+    
+Console.Write ":>"  
+let mutable input = Console.ReadLine()  
+
+let remoteCall (sql:string) = 
+    let cNode = new OtpSelf("querlyclientnode")
+    let sNode = new OtpPeer("querly@dmohl-PC")
+    let connection = cNode.connect(sNode)
+    let arguments = [|new Erlang.String(sql)|] : Erlang.Object[]
+    do connection.sendRPC("querly_client", "sql_query", arguments) 
+    connection.receiveRPC().ToString()
+   
+while input <> "quit"  
+    do  
+    if input <> "quit"   
+    then
+        let result = remoteCall input  
+        Console.Write(result)
+        Console.Write "\n:>"   
+        input <- Console.ReadLine()
+
