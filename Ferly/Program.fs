1a7fecd06538f69559ea128698d3bc3491a23ff6
Dan Mohl <dmohl@yahoo.com>
2010-05-03 21:44:46 -0500

    A little refactoring

--- Ferly/Program.fs
+++ Ferly/Program.fs
@@ -7,23 +7,23 @@ Console.Write ":>"
 let mutable input = Console.ReadLine()  
 
 let erlangCookie = "supersecretcookie"
+let peerNode = "querly@dmohl-PC"
 
 let connection =
     let cNode = new OtpSelf("querlyclientnode", erlangCookie)
-    let sNode = new OtpPeer("querly@dmohl-PC")
+    let sNode = new OtpPeer(peerNode)
     cNode.connect(sNode)
 
 let RemoteCall (sql:string) = 
     let arguments = [|new Erlang.String(sql)|] : Erlang.Object[]
     do connection.sendRPC("querly_client", "sql_query", arguments) 
     connection.receiveRPC().ToString()
-   
+
 while input <> "quit"  
-    do  
-    if input <> "quit"   
-    then
-        let result = RemoteCall input  
-        Console.Write(result)
-        Console.Write "\n:>"   
-        input <- Console.ReadLine()
+    do match input with
+       | _ when input = "quit" -> do connection.close()
+       | _ -> 
+             Console.Write(RemoteCall input)
+             Console.Write "\n\n:>"   
+             input <- Console.ReadLine()
 
